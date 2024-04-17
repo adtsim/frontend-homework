2eccbc58858f1a4576f56739dd616b082b56d849
Aditi Simha <adisimha@pdx.edu>
2024-04-17 15:15:24 -0700

    Homework 1: Setup and Review
    Submission for HW1, including solutions for both Flexbox Froggy and the Palindrome program.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,30 @@
-const elem = document.querySelector('input');
+const inputElement = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+inputElement.addEventListener("input", isNumPalindrome);
+
+function isNumPalindrome() {
+  var num = document.getElementById("number").value;
+
+  result.textContent = "";
+
+  if (num === "") {
+    return;
+  }
+
+  if (num >= 0) {
+    var reversedNum = parseInt(num.toString().split("").reverse().join(""));
+
+    if (parseInt(num) === reversedNum) {
+      document.getElementById("result").textContent =
+        "Yes. This is a palindrome!";
+      document.getElementById("result").style.color = "green";
+    } else {
+      document.getElementById("result").textContent = "No. Try again.";
+      document.getElementById("result").style.color = "red";
+    }
+  } else {
+    document.getElementById("result").textContent =
+      "Please enter a positive number.";
+    document.getElementById("result").style.color = "red";
+  }
+}
